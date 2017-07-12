7695c0f447607a56361b448b75b5e3eba2d3258a
David Duffrin <david.duffrin15@ncf.edu>
2017-07-12 12:05:18 -0400

    finished lesson 1

--- work/test1.py
+++ work/test1.py
@@ -1,27 +1,29 @@
 from math import log
 
-lambda = 0.95 # 1 - lambda = 0.05
+lmd = 0.95 # 1 - lmd = 0.05
 V = 1000000
 W = 0
 H = 0
+unk = 0
 probabilities = dict()
 
 with open('../test/01-model-file.txt', 'r') as model_file:
     for line in model_file.read().split('\n')[:-1]:
         k, v = line.split('\t')
-        probabilities[k] = v
+        probabilities[k] = float(v)
 
-for each line in test_file:
-    split line into an array of words
-    append “</s>” to the end of words
-    for each w in words
-        add 1 to W
-        set P= λunk / V
-        if probabilities[w] exists
-            set P += λ1 * probabilities[w]
-        else
-            add 1 to unk
-            add -log(P, 2) to H
+with open('../test/01-test-input.txt', 'r') as test_file:
+    for line in test_file.read().split('\n')[:-1]:
+        words = line.split()
+        words.append("</s>")
+        for w in words:
+            W += 1
+            P = (1 - lmd) / V
+            if probabilities.get(w, None) != None:
+                P += lmd * probabilities[w]
+            else:
+                unk += 1
+            H += -log(P, 2)
 
-print “entropy = ”+H/W
-print “coverage = ” + (W-unk)/W
+print "entropy = " + str(H/W) # currently, H is incorrect
+print "coverage = " + str(float(W-unk)/W)
