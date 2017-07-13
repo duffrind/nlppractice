2d5d209fcebc7b6599a03251556feac33be88fd9
David Duffrin <david.duffrin15@ncf.edu>
2017-07-13 15:01:48 -0400

    lesson 2 train is now ngram compatible

--- work/train2.py
+++ work/train2.py
@@ -1,23 +1,31 @@
 counts = dict()
 context_counts = dict()
 
-test = False
+test = True
 file_name = '../data/wiki-en-train.word'
 if test:
     file_name = '../test/02-train-input.txt'
 
+n = 2 # n-gram: change to anything >= 0
+
 with open(file_name, 'r') as training_file:
     for line in training_file.read().split('\n')[:-1]:
-        words = ['<s>'] + line.split() + ['</s>']
-        for i in range(1, len(words)): # Note: starting at 1, after <s>
-            counts[words[i - 1] + ' ' + words[i]] = counts.get(words[i - 1] + ' ' + words[i], 0) + 1
-            context_counts[words[i - 1]] = context_counts.get(words[i - 1], 0) + 1
+        words = ['<s>'] * (n - 1) + line.split() + ['</s>']
+        for i in range(n - 1, len(words)): # Note: starting at 1, after <s>
+            ngram = list()
+            for j in range(i - (n-1), i):
+                ngram.append(words[j])
+            ngram_joined = ' '.join(ngram)
+            counts[ngram_joined] = counts.get(ngram_joined, 0) + 1
+            _ = ngram.pop()
+            ngram_joined = ' '.join(ngram)
+            context_counts[ngram_joined] = context_counts.get(ngram_joined, 0) + 1
             counts[words[i]] = counts.get(words[i], 0) + 1
             context_counts[''] = context_counts.get('', 0) + 1
 
 with open('../test/02-model-file.txt', 'w') as model_file:
     for ngram, count in counts.iteritems():
-        words = ngram.split(' ')
+        words = ngram.split(' ') # split on space is implied, but explicit is better than implicit (?)
         _ = words.pop()
         context = ' '.join(words)
         probability = float(counts[ngram]) / context_counts[context]
@@ -30,3 +38,7 @@ with open('../test/02-model-file.txt', 'w') as model_file:
 # Challenge:
 # Use Witten-Bell smoothing (Linear interpolation is easier)
 # Create a program that works with any n (not just bigram)
+    # add </s>
+    # input n (if n <= 0, error)
+    # add n-1 <s> to beginning
+    #
